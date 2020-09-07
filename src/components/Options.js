06c0abab5e7ab1c6c8042bbae954aa96d01b79c0
Tamir <kaliftamir@gmail.com>
2020-09-07 16:25:14 +0300

    add controlled component to Options

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,52 +1,57 @@
 import React, { useState } from 'react';
 import { Modal,Button,Row,Col,Form,InputGroup } from 'react-bootstrap';
 
-const Options = () => {
+function Options() {
    
 
     const blankOption = { name: '' };
-    const [optionState, setOptionState] = useState([
-        { ...blankOption },
-    ]);
+    const [optionsState, setOptionsState] = useState([{ ...blankOption },]);
+    console.log(optionsState[0])
 
-    const addOption = () => {
-        setOptionState([...optionState, { ...blankOption }]);
+    function addOption() {
+        setOptionsState([...optionsState, { ...blankOption }]);
+    };
+
+    // controlled component
+    function handleOptiontChange(e) {
+        const updatedOptions = [...optionsState];
+        updatedOptions[e.target.dataset.idx][e.target.className] = e.target.value;
+        setOptionsState(updatedOptions);
     };
 
     return (
-        <form>
-
+        <div>
             
             {
-                optionState.map((val, idx) => {
+                optionsState.map((val, idx) => {
                     const optionId = `name-${idx}`;
                     
                     return (
                         
                         <Row key={`option-${idx}`}>
                             <Col sm={10}>
-                            <Form.Label htmlFor={optionId}>{`Option #${idx + 1}:`}</Form.Label>
-                            
-                                <Form.Control
-                                    type="text"
-                                    name={optionId}
-                                    data-idx={idx}
-                                    id={optionId}
-                                    className="name"
-                                />
+                            <Form.Group>    
+                                <Form.Label htmlFor={optionId}>{`Option #${idx + 1}:`}</Form.Label>
+                                
+                                    <Form.Control
+                                        type="text"
+                                        name={optionId}
+                                        data-idx={idx}
+                                        id={optionId}
+                                        className="name"
+                                        onChange={handleOptiontChange}
+                                    />
+                            </Form.Group>
                             </Col>
+                            
                                                       
                         </Row>
                     );
                 })
             }
-            <input id="add-input"
-                type="button"
-                value="+ Add option"
-                onClick={addOption}
-            />
-            {/* <input type="submit" value="Submit" /> */}
-        </form>
+            <input id="add-input" type="button"  value="+ Add option" onClick={addOption} />
+            
+        </div>
     );
 };
 
