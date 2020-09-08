c1fbe0ca47156004c56d77dfde6c9234d143270b
Tamir <kaliftamir@gmail.com>
2020-09-08 10:36:23 +0300

    Updates

---
+++ precache-manifest.356111fb436a9bfd2eafc652f7fad47c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455299195214bc54ca9c28d7fdd0d92a",
+    "url": "/my-house-committee/index.html"
+  },
+  {
+    "revision": "b870c1d20bd8bcd07787",
+    "url": "/my-house-committee/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "83761878924f9648f49f",
+    "url": "/my-house-committee/static/css/main.f05bf562.chunk.css"
+  },
+  {
+    "revision": "b870c1d20bd8bcd07787",
+    "url": "/my-house-committee/static/js/2.0d336648.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/my-house-committee/static/js/2.0d336648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83761878924f9648f49f",
+    "url": "/my-house-committee/static/js/main.f0f0fd43.chunk.js"
+  },
+  {
+    "revision": "9fc6fe5f84b5b86401a4",
+    "url": "/my-house-committee/static/js/runtime-main.329b954a.js"
+  }
+]);
